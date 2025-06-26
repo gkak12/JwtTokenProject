e23c5cb6eaec46c8b3f736570c60ff299cd086d7
hyunjun <hyunjun5681@kakao.com>
2025-06-26 21:08:33 +0900

    Oauth2 SuccessHandler 계정 정보 저장 로직 수정

--- src/main/kotlin/com/jwt/oauth2/handler/OAuth2AuthenticationSuccessHandler.kt
+++ src/main/kotlin/com/jwt/oauth2/handler/OAuth2AuthenticationSuccessHandler.kt
@@ -1,25 +1,17 @@
 package com.jwt.oauth2.handler
 
-import com.jwt.comm.AccountEnums
-import com.jwt.comm.JwtEnums
-import com.jwt.comm.JwtUtil
-import com.jwt.comm.RedisComponent
-import com.jwt.user.domain.entity.User
-import com.jwt.user.repository.UserRepository
+import com.jwt.oauth2.service.OAuth2Service
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import jakarta.transaction.Transactional
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
-import java.util.Base64
+import java.util.*
 
 @Component
 class OAuth2AuthenticationSuccessHandler(
-    private val jwtUtil: JwtUtil,
-    private val userRepository: UserRepository,
-    private val redisComponent: RedisComponent
+    private val oAuth2Service: OAuth2Service
 ): AuthenticationSuccessHandler {
 
     override fun onAuthenticationSuccess(
@@ -31,19 +23,9 @@ class OAuth2AuthenticationSuccessHandler(
         val email = principal.getAttribute<String>("email")
         val name = principal.getAttribute<String>("name")
 
-        val refreshToken = jwtUtil.createToken(JwtEnums.REFRESH_TYPE.value, email)
-
-        findOrCreateUser(email, name)
-        redisComponent.setRefreshToken(email+JwtEnums.TOKEN_KEY.value, refreshToken)
+        oAuth2Service.createUserInfo(email, name)
 
         val encodedEmail = Base64.getEncoder().encodeToString(email.toByteArray())
         response.sendRedirect("/oauth2/me?id=$encodedEmail")
-    }
-
-    @Transactional
-    fun findOrCreateUser(email: String, name: String) {
-        userRepository.findById(email) ?: userRepository.save(
-            User(email, "", name, AccountEnums.ROLE_USER.value)
-        )
     }
 }
--- src/main/kotlin/com/jwt/oauth2/service/OAuth2Service.kt
+++ src/main/kotlin/com/jwt/oauth2/service/OAuth2Service.kt
@@ -1,18 +1,38 @@
 package com.jwt.oauth2.service
 
+import com.jwt.comm.AccountEnums
 import com.jwt.comm.JwtEnums
 import com.jwt.comm.JwtUtil
+import com.jwt.comm.RedisComponent
+import com.jwt.user.domain.entity.User
 import com.jwt.user.domain.response.ResponseJwtTokenDto
+import com.jwt.user.repository.UserRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import java.util.Base64
 
 @Service
 class OAuth2Service (
-    private val jwtUtil: JwtUtil
+    private val jwtUtil: JwtUtil,
+    private val userRepository: UserRepository,
+    private val redisComponent: RedisComponent
 ){
     fun createToken(encodedId: String): ResponseJwtTokenDto{
         val id = String(Base64.getDecoder().decode(encodedId))
         val accessToken = jwtUtil.createToken(JwtEnums.ACCESS_TYPE.value, id)
+
         return ResponseJwtTokenDto(accessToken)
+    }
+
+    @Transactional
+    fun createUserInfo(email: String, name: String){
+        val refreshToken = jwtUtil.createToken(JwtEnums.REFRESH_TYPE.value, email)
+        redisComponent.setRefreshToken(email+JwtEnums.TOKEN_KEY.value, refreshToken)
+
+        userRepository.findById(email).orElseGet{
+            userRepository.save(
+                User(email, "", name, AccountEnums.ROLE_USER.value)
+            )
+        }
     }
 }
