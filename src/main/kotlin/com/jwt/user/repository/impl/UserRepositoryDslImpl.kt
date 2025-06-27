8d12eb874306943c6f2253eca0276760d31c0f3f
hyunjun <hyunjun5681@kakao.com>
2025-06-27 12:49:41 +0900

    ConditionBuilderUtil 적용

--- src/main/kotlin/com/jwt/user/repository/impl/UserRepositoryDslImpl.kt
+++ src/main/kotlin/com/jwt/user/repository/impl/UserRepositoryDslImpl.kt
@@ -1,9 +1,11 @@
 package com.jwt.user.repository.impl
 
+import com.jwt.comm.util.ConditionBuilderUtil
 import com.jwt.user.domain.entity.QUser.user
 import com.jwt.user.domain.entity.User
 import com.jwt.user.domain.request.RequestUserSearchDto
 import com.jwt.user.repository.UserRepositoryDsl
+import com.querydsl.core.BooleanBuilder
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Page
@@ -35,9 +37,15 @@ class UserRepositoryDslImpl(
             pageSize
         )
 
+        val booleanBuilder = BooleanBuilder()
+        booleanBuilder
+            .and(ConditionBuilderUtil.buildEquals(user.id, userSearchDto.id))
+            .and(ConditionBuilderUtil.buildStringLike(user.name, userSearchDto.name))
+
         val list = jpaQueryFactory
             .select(user)
             .from(user)
+            .where(booleanBuilder)
             .offset(pageable.offset)
             .limit(pageable.pageSize.toLong())
             .fetch()
@@ -45,9 +53,9 @@ class UserRepositoryDslImpl(
         val count = jpaQueryFactory
             .select(user.count())
             .from(user)
+            .where(booleanBuilder)
             .fetchOne() ?: 0L
 
         return PageImpl(list, pageable, count)
     }
 }
-
