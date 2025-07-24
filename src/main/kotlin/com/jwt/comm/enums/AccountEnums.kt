1c371a991f7faf99c708720c5dc02de4b648068e
hyunjun <hyunjun5681@kakao.com>
2025-07-24 20:26:57 +0900

    루트 관리자 권한 Enum 변경

--- src/main/kotlin/com/jwt/comm/enums/AccountEnums.kt
+++ src/main/kotlin/com/jwt/comm/enums/AccountEnums.kt
@@ -4,5 +4,5 @@ enum class AccountEnums (val value: String, val msg: String) {
     REDIS_INFO(":REDIS_INFO", "REDIS 계정 키"),
     ROLE_USER("ROLE_USER", "일반 사용자 권한"),
     ROLE_ADMIN("ROLE_ADMIN", "일반 관리자 권한"),
-    ROLE_SUPER_ADMIN("ROLE_ADMIN", "루트 관리자 권한")
+    ROLE_ROOT_ADMIN("ROLE_ROOT_ADMIN", "루트 관리자 권한")
 }
